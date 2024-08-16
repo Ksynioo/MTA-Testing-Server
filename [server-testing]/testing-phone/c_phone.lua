63e6cede87239fd0a3ae7d7e3218203300672de7
Ksynioo <krzstr98@gmail.com>
2024-08-16 19:46:24 +0200

    Minor Changes - Changed image file format from PNG to JPG - Created folder for police radio - Created folder for police tablet

--- [server-testing]/testing-phone/c_phone.lua
+++ [server-testing]/testing-phone/c_phone.lua
@@ -1,12 +1,36 @@
 local sx, sy = guiGetScreenSize()
+local font = guiCreateFont("font/font.ttf", 12)
 
 addEventHandler("onClientResourceStart", resourceRoot,
     function()
-    phone = guiCreateStaticImage(sx-300, sy-600, 300, 600, "img/phone.dds", false)
+    phone = guiCreateStaticImage(sx-300, sy-525, 250, 500, "img/phone.jpg", false)
     guiSetVisible(phone,false)
-    back = guiCreateStaticImage(75, 559, 150, 4, "img/back.dds", false, phone)
+
+    contacts = guiCreateStaticImage(18, 55, 48, 48  , "img/contacts.jpg", false, phone)
+    contacts = guiCreateStaticImage(73, 55, 48, 48  , "img/messages.jpg", false, phone)
+    contacts = guiCreateStaticImage(128, 55, 48, 48  , "img/appstore.jpg", false, phone)
+    contacts = guiCreateStaticImage(183, 55, 48, 48  , "img/gmail.jpg", false, phone)
+
+    label1 = guiCreateLabel(165, 15, 246,30, "lllll", false, phone)
+			guiSetFont(label1, font)
+      
+		local timehour, timeminute = getTime()
+		if timehour < 10 then
+			timehour = "0"..timehour
+		end
+		if timeminute < 10 then
+			timeminute = "0"..timeminute
+    end
+
+		label2 = guiCreateLabel(30, 15, 246,30, timehour..":"..timeminute, false, phone)
+			guiSetFont(label2, font)
+		label3 = guiCreateLabel(190, 15, 246,30, "100%", false, phone)
+			guiSetFont(label3, font)
+
+    back = guiCreateStaticImage(75, 480, 100, 5, "img/back.jpg", false, phone)
     addEventHandler ( "onClientGUIClick", back, phone_close, false )
 end)
+
 
 function phone_open(button)
     if button == "mouse3" then
